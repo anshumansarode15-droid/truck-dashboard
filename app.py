75b9b02bb5f02f4db3db6c51094fe33282d2aa2d
anshumansarode15-droid <anshumansarode15@gmail.com>
2026-07-09 16:03:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -213,5 +213,4 @@ else:
                                 weight=4,
                                 opacity=0.8
                             ).add_to(base_map)
-
-finally
+                            finally
